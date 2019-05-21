f7a1b622869fc7e93f1c7c1a9b94a7bc6d74e529
sam18892 <sam.yohind@gmail.com>
2019-05-22 04:02:32 +0530

    Refactoring viewModel

--- app/src/test/java/com/project/tennis/TennisViewModelUnitTest.kt
+++ app/src/test/java/com/project/tennis/TennisViewModelUnitTest.kt
@@ -24,112 +24,83 @@ class TennisViewModelUnitTest {
 
     @Test
     fun testPlayerOneWinsFirstBall() {
-        tennisViewModel.playerOneScores()
+        recordPoints(1, 0)
         assertEquals(TennisViewModel.POINT_ONE + " - " + TennisViewModel.POINT_LOVE,
             tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testPlayerTwoWinsFirstBall() {
-        tennisViewModel.playerTwoScores()
+        recordPoints(0, 1)
         assertEquals(TennisViewModel.POINT_LOVE + " - " + TennisViewModel.POINT_ONE,
             tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testIfBothPlayersScoredOnePointEach() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerTwoScores()
+        recordPoints(1, 1)
         assertEquals(TennisViewModel.POINT_ONE + " " + TennisViewModel.POINT_EQUAL,
             tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testIfPlayerOneHasScoredFirstTwoPoints() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
+        recordPoints(2, 0)
         assertEquals(TennisViewModel.POINT_TWO + " - " + TennisViewModel.POINT_LOVE,
             tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testIfPlayerOneHasScoredFirstThreePoints() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
+        recordPoints(3, 0)
         assertEquals(TennisViewModel.POINT_THREE + " - " + TennisViewModel.POINT_LOVE, tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testShouldReturnAllInScoreIfBothPlayersScoresEqualPoints() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
+        recordPoints(2, 2)
         assertEquals(TennisViewModel.POINT_TWO + " " + TennisViewModel.POINT_EQUAL, tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testShouldReturnDeuceIfBothPlayersScoresThreePointsEach() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
+        recordPoints(3, 3)
         assertEquals(TennisViewModel.MATCH_STATUS_DEUCE, tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testShouldReturnAdvantagePlayerTwoIfTheyScoreAPointAfterDeuce() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
+        recordPoints(3, 4)
         assertEquals(TennisViewModel.MATCH_STATUS_ADVANTAGE + " " + Players.PLAYER_TWO_NAME,
             tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testShouldReturnAdvantagePlayerOneIfTheyScoreAPointAfterDeuce() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerOneScores()
+        recordPoints(4, 3)
         assertEquals(TennisViewModel.MATCH_STATUS_ADVANTAGE + " " + Players.PLAYER_ONE_NAME,
             tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testShouldReturnPlayerTwoWinsIfTheyScoreAPointAfterAdvantage() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
+        recordPoints(3, 5)
         assertEquals(Players.PLAYER_TWO_NAME + " " + TennisViewModel.MATCH_STATUS_WON,
             tennisViewModel.getMatchScore())
     }
 
     @Test
     fun testShouldReturnPlayerOneWinsIfTheyScoreAPointAfterAdvantage() {
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerTwoScores()
-        tennisViewModel.playerOneScores()
-        tennisViewModel.playerOneScores()
+        recordPoints(5, 3)
         assertEquals(Players.PLAYER_ONE_NAME + " " + TennisViewModel.MATCH_STATUS_WON, tennisViewModel.getMatchScore())
+    }
+
+    private fun recordPoints(playerOne: Int, playerTwo: Int){
+        for(i in 1..playerOne){
+            tennisViewModel.playerOneScores()
+        }
+        for (i in 1..playerTwo){
+            tennisViewModel.playerTwoScores()
+        }
     }
 }
